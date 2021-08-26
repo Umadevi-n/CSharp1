cdb99da2be5a7e2cde60f7d10cbbe43174acc0a8
Quadrant <Quadrant@DESKTOP-FEKK5US>
2021-08-26 21:12:24 +0530

    Push

---
+++ Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Dictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Dint = new Dictionary<int, string>();
+            // Add some elements to the dictionary.
+            Dint.Add(1, "Student 1");
+            Dint.Add(2, "Student 2");
+            Dint.Add(3, "Student 3");
+            Dint.Add(4, "Student 4");
+            // The Add method throws an exception if the new key is  
+            // already in the dictionary. 
+            //Dint.Add(3, "Student 5");
+            // The Item property is another name for the indexer, so you  
+            // can omit its name when accessing elements. 
+            Console.WriteLine("For key = \"1\", value = {0}.", Dint[1]);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+
+namespace HashTable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable obj = new Hashtable();
+
+            obj.Add(1, "Uma");
+            obj.Add(2, "Devi");
+            obj.Add(3, "Mantena");
+
+            for (int i = 1; i <= obj.Count; i++)
+            {
+                Console.WriteLine("The value for the Key is {0}", obj[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
